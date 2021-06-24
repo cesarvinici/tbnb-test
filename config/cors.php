568838942c44a44ea2dd9d53c9076b46c90c39a7
Cesar Vinicius <cesarvinici@gmail.com>
2021-06-23 22:37:15 -0300

    adding heroku front to allowed origins

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://cesarvss-tbnb-test.herokuapp.com'],
 
     'allowed_origins_patterns' => [],
 
